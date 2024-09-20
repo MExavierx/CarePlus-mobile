bfe61fc98cbaee251058279b13c2dda62830ea78
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2024-09-20 15:54:55 -0300

    Criei prontuario

--- TESTE/app/src/main/java/com/example/testecareplus/TelaCadastro.java
+++ TESTE/app/src/main/java/com/example/testecareplus/TelaCadastro.java
@@ -2,7 +2,11 @@ package com.example.testecareplus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class TelaCadastro extends AppCompatActivity {
 
@@ -12,6 +16,27 @@ public class TelaCadastro extends AppCompatActivity {
         setContentView(R.layout.activity_tela_cadastro);
 
 
-        //Funcionalidade dos botões 
+        //Funcionalidade dos botões
+        EditText email = findViewById(R.id.campoEmailCadastro);
+        EditText usuario = findViewById(R.id.campoUsuario);
+        EditText senha = findViewById(R.id.campoSenhaCadastro);
+        Button cadastrar = findViewById(R.id.btCadastrarCadastro);
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent(TelaCadastro.this, HomeFragment.class);
+
+                startActivity(intent);
+
+            }
+
+        });
+
+
+
     }
 }
